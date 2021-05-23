0800066205f46291b93298df7ab548346c6c024a
Anand Kuchibotla <akuchibotla@gmail.com>
2021-05-23 09:46:49 -0700

    modified db client interface

--- api/order_book_service/db/db_client.py
+++ api/order_book_service/db/db_client.py
@@ -2,11 +2,11 @@
 Interface for database client (can plug in any db solution)
 """
 class DBClient():
-    def __init__(self, conn_str=None):
-        raise NotImplementedError('constructor not implemented')
-
     def __update_record__(self, security, side_key, spread_side_records):
         raise NotImplementedError('__update_record__ not implemented')
+
+    def __initialize_security_records__(self, security):
+        raise NotImplementedError('__initialize_security_records__ not implemented')
     
     def get_orders(self, security):
         raise NotImplementedError('get_orders not implemented')
